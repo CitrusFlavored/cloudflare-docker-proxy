b3d382c724c45623e7268bfe6fe8de808af2a324
晴风 <54226117+CitrusFlavored@users.noreply.github.com>
2024-06-24 17:05:22 +0800

    Update index.js
    Change the original domain name to mine

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.clearwind.cc": dockerHub,
+  //"quay.clearwind.cc": "https://quay.io",
+  //"gcr.clearwind.cc": "https://gcr.io",
+  //"k8s-gcr.clearwind.cc": "https://k8s.gcr.io",
+  //"k8s.clearwind.cc": "https://registry.k8s.io",
+  //"ghcr.clearwind.cc": "https://ghcr.io",
+  //"cloudsmith.clearwind.cc": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-clearwind.cc": dockerHub,
 };
 
 function routeByHosts(host) {
